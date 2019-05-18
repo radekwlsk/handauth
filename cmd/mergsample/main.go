91a032be3a991fb1ce11f8e06d0700d6e16797e7
Radosław Kowalski <radek.kwlsk@gmail.com>
2019-05-18 15:29:13 +0200

    draw center of mass for each sample, use distinct color for each sample

--- cmd/mergsample/main.go
+++ cmd/mergsample/main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"github.com/radekwlsk/handauth/cmd"
 	"github.com/radekwlsk/handauth/cmd/flags"
+	"gocv.io/x/gocv"
+	"golang.org/x/image/draw"
+	"gonum.org/v1/plot/palette/brewer"
 	"image"
 	"image/color"
 	imgdraw "image/draw"
@@ -18,10 +21,10 @@ import (
 const TestStartTimeFormat = "20060201-150405"
 
 var (
-	userId       int
-	start        time.Time
-	startString  string
-	samplesUsers map[int][]int
+	userId         int
+	start          time.Time
+	drawMassCenter bool
+	samplesUsers   map[int][]int
 )
 
 type mask struct {
@@ -45,45 +48,86 @@ func (m *mask) At(x, y int) color.Color {
 	return color.Alpha{0}
 }
 
-func mergedSamples(id int) *image.Gray {
+func drawDot(dst *image.RGBA, center image.Point, c color.RGBA) {
+	for _, x := range []int{center.X - 1, center.X, center.X + 1} {
+		for _, y := range []int{center.Y - 1, center.Y, center.Y + 1} {
+			p := image.Point{X: x, Y: y}
+			if p.In(dst.Bounds()) {
+				dst.SetRGBA(p.X, p.Y, c)
+			}
+		}
+	}
+}
+
+var blackRGBA = color.RGBA{A: 255}
+var redRGBA = color.RGBA{R: 255, A: 255}
+
+func mergedSamples(id int) *image.RGBA {
 	sample, err := cmd.ReadUserSample(uint8(id), uint8(id), uint8(samplesUsers[id][0]))
 	if err != nil {
 		panic(err)
 	}
-	ratio := sample.Sample().Ratio()
 	sample.Preprocess()
 	sampleMat := sample.Sample().Mat()
 	img, err := sampleMat.ToImage()
 	if err != nil {
 		panic(err)
 	}
-	mergedImg := image.NewGray(img.Bounds())
-	for _, sampleId := range samplesUsers[id] {
+	dst := image.NewRGBA(img.Bounds())
+	draw.Draw(dst, dst.Bounds(), &image.Uniform{C: blackRGBA}, image.ZP, draw.Src)
+
+	palSize := len(samplesUsers[id])
+	if palSize > 12 {
+		palSize = 12
+	}
+	pal, err := brewer.GetPalette(brewer.TypeQualitative, "Set3", palSize)
+	if err != nil {
+		log.Fatal(err)
+	}
+	colors := pal.Colors()
+
+	var massCenters []image.Point
+	for i, sampleId := range samplesUsers[id] {
 		sample, err := cmd.ReadUserSample(uint8(id), uint8(id), uint8(sampleId))
 		if err != nil {
 			panic(err)
 		}
-		sample.Sample().Preprocess(ratio)
-		sampleMat := sample.Sample().Mat()
-		img2, err := sampleMat.ToImage()
+		sample.Preprocess()
+		var sampleMat gocv.Mat
+		if drawMassCenter {
+			massCenters = append(massCenters, sample.Sample().CenterOfMass())
+		}
+		sampleMat = sample.Sample().Mat()
+		src, err := sampleMat.ToImage()
 		if err != nil {
 			panic(err)
 		}
-		imgdraw.DrawMask(mergedImg, img2.Bounds(), img2, image.ZP, &mask{img2}, img2.Bounds().Min, imgdraw.Over)
+		if dst.Bounds().In(src.Bounds()) {
+			newDst := image.NewRGBA(src.Bounds())
+			draw.Draw(newDst, newDst.Bounds(), &image.Uniform{C: blackRGBA}, image.ZP, draw.Src)
+			draw.Draw(newDst, dst.Bounds(), dst, image.ZP, draw.Src)
+			dst = newDst
+		}
+		c := colors[i%12]
+		msk := mask{src}
+		imgdraw.DrawMask(dst, src.Bounds(), &image.Uniform{C: c}, image.ZP, &msk, image.ZP, imgdraw.Over)
 	}
-	return mergedImg
+	for _, cm := range massCenters {
+		drawDot(dst, cm, redRGBA)
+	}
+	return dst
 }
 
 func main() {
 	flag.IntVar(&userId, "u", 1, "user to generate variation within user's samples")
+	flag.BoolVar(&drawMassCenter, "cm", false, "draw center of mass of the samples")
 	flag.Parse()
 
 	start = time.Now()
-	startString = start.Format(TestStartTimeFormat)
 
 	samplesUsers = cmd.GenuineUsers(true)
 	if _, ok := samplesUsers[userId]; userId > 0 && !ok {
-		log.Fatal("No such user")
+		log.Fatalf("No user %03d", userId)
 	}
 
 	start := time.Now()
